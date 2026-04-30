eacd9932b0d3dfacbe7f7aef023df0dd46379028
serhat-bca <sersen@bergen.org>
2026-04-30 13:39:45 -0400

    initial data added.

---
+++ seinfeld/src/App.jsx
@@ -0,0 +1,41 @@
+const App = () => {
+  const seinfeld = {
+    id: 1,
+    title: "Seinfeld Season One",
+    episodes: [
+      {
+        id: 1,
+        title: "The Seinfeld Chronicles",
+        views: 15300000,
+      },
+      {
+        id: 2,
+        title: "The Stake Out",
+        views: 14200000,
+      },
+      {
+        id: 3,
+        title: "The Robbery",
+        views: 13800000,
+      },
+      {
+        id: 4,
+        title: "Male Unbonding",
+        views: 13100000,
+      },
+      {
+        id: 5,
+        title: "The Stock Tip",
+        views: 14500000,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <h2>Seinfeld Show</h2>
+    </>
+  );
+};
+
+export default App;
